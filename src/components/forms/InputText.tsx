37506b0737427155fd31c20a79ba4a66e94cb3c0
Eagles <okponobiemmanuel@gmail.com>
2024-08-28 22:01:29 +0100

    Update on form

--- src/components/forms/InputText.tsx
+++ src/components/forms/InputText.tsx
@@ -1,18 +1,24 @@
+import { ChangeEvent } from "react";
+
 interface IProps {
   type: string;
   name: string | number;
-  onChange: () => void;
+  onChange: (event: string | number) => void;
   required: boolean;
 }
 
 export function App(props: IProps) {
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    props.onChange(e.target.value)
+  }
+
   return (
     <div>
       <input
         id="name"
         type="text"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
+        value={props.name}
+        onChange={(e) => handleChange(e)}
         required
         className="px-3 py-2 rounded-md border border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-500"
       />
